082498eea15708bc154a437ea155499f9053d423
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-05-02 09:48:36 +0000

    added admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["BASIC_AUTH_NAME"], password: ENV["BASIC_AUTH_PASS"]
 
   def show
+    @products_in_db = Product.all.length
+
+    @categories_in_db = Category.all.length
   end
 end
